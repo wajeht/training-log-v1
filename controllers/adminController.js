e73a6cbe282c333163ae7f8c09a685911257ea2c
wajeht <zombyard@gmail.com>
2021-08-12 23:27:45 -0500

    bug: fix ffmpeg modulle for prod server

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,8 @@
 // Utils
 const fs = require('fs');
 const path = require('path');
-const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');;
 const { root } = require('../util/directory.js');
 
 // Models
