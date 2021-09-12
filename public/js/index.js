17538ef51ea11078959d0330b88eaeccc8ddb88f
wajeht <zombyard@gmail.com>
2021-09-12 01:22:38 -0500

    feat: add lifting log under add-video uplaod

--- public/js/index.js
+++ public/js/index.js
@@ -126,40 +126,7 @@ document.addEventListener('DOMContentLoaded', function () {
   M.Modal.init(elems);
 });
 
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: Utils.numbers(NUMBER_CFG),
-      borderColor: Utils.CHART_COLORS.red,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-    },
-    {
-      label: 'Dataset 2',
-      data: Utils.numbers(NUMBER_CFG),
-      borderColor: Utils.CHART_COLORS.blue,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-    },
-  ],
-};
-
-const config = {
-  type: 'line',
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart',
-      },
-    },
-  },
-};
-
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, config);
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('select');
+  M.FormSelect.init(elems);
+});
