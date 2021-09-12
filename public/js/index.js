9f7f943f0c3958d3109668ab3db9b3e0cbc126bc
wajeht <zombyard@gmail.com>
2021-09-12 00:18:08 -0500

    bug: add narrrow width on cta text description

--- public/js/index.js
+++ public/js/index.js
@@ -125,3 +125,41 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.modal');
   M.Modal.init(elems);
 });
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: Utils.numbers(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.red,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+    },
+    {
+      label: 'Dataset 2',
+      data: Utils.numbers(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.blue,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+    },
+  ],
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  },
+};
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, config);
